4b9ee01bef54534b22b94f7463ef167c784a546f
Mathew Lister <36172965+AngryPorkchop11@users.noreply.github.com>
2019-07-10 20:09:48 -0700

    Update HangMan.cpp

--- HangMan/HangMan/HangMan.cpp
+++ HangMan/HangMan/HangMan.cpp
@@ -1,57 +1,78 @@
-//
 #include <iostream>
 #include <string>
 #include <iomanip>
 
-using namespace std;
-
 #include "HangMan.h"
 
-PlayHangman::PlayHangman(string& answer)
+PlayHangman::PlayHangman(string& answer) // Constructor
 {
 	_size = answer.length();
 	_guessCount = 0;
 	_answer = answer;
-	// Create dynamic array for guesses
-	_guess = new char[_size + 1];
-	for(int i = 0; i < _size+1; i++)
+	// Create dynamic array for wrong and right
+	_wrong = new char[_endGame];
+	for (int i = 0; i < _endGame + 1; i++)
 	{
-		_guess[i] = '0'; 
+		_wrong[i] = '0';
 	}
-	_right = new char[_size+1];
-	for (int i = 0; i < _size+1; i++)
+
+	_right = new char[_size];
+	for (int i = 0; i < _size + 1; i++)
 	{
 		_right[i] = '_';
 	}
 }
 
-int PlayHangman::GuessCheck(char& guess) //Adds guess as a correct letter or a wrong guessed letter
+bool PlayHangman::GuessCheck(char& guess) //Adds guess as a correct letter or a wrong guessed letter // T=GuessRight F=GuessWrong
 {
+	bool guessFound = false;
 	if (Lose() == false)
 	{
 		for (int i = 0; i < _size; i++)
 		{
 			if (_answer[i] == guess)
 			{
-				_right[i] == guess;
-				_guessCount++;
-				return 1;
+				_right[i] = guess;
+				guessFound = true;
 			}
-			if (_guess[i] == '0')
+		}
+		if (guessFound == false)
+		{
+			for (int i = 0; i < _endGame; i++)
 			{
-				_guess[i] = guess;
-				_guessCount++;
-				return -1;
+				if (_wrong[i] == '0' && checkMultiple(guess) == 1)
+				{
+					_wrong[i] = guess;
+					_guessCount++;
+				}
 			}
 		}
 	}
+	return guessFound;
+}
+
+int PlayHangman::checkMultiple(char guess) // Checks if player has already guessed a char
+{
+	for (int i = 0; i < _size; i++)
+		if (_right[i] == guess)
+			return -1;
+	for (int i = 0; i < _endGame; i++)
+		if (_wrong[i] == guess)
+			return -1;
+	return 1;
 }
 
 void PlayHangman::ShowGuesses(ostream& fout)
 {
 	for (int i = 0; i < _size; i++)
 	{
-		fout << " " << _guess[i];
+		if (_right[i] != '_')
+			fout << " " << _right[i];
+	}
+	for (int i = 0; i < _endGame; i++)
+	{
+		if (_wrong[i] != '0')
+		fout << " " << _wrong[i];
 	}
 }
 
@@ -72,15 +93,15 @@ bool PlayHangman::Win()
 	return true;
 }
 
-void PlayHangman::_rightAsString(ostream & fout)
+void PlayHangman::_rightAsString(ostream& fout)
 {
 	for (int i = 0; i < _size; i++)
 	{
-		fout << _right[i] << " ";
+		fout << _right[i];
 	}
 }
 
-void PlayHangman::Draw(ostream & fout)
+void PlayHangman::Draw(ostream& fout)
 {
 	if (_guessCount == 0)
 	{
@@ -126,16 +147,16 @@ void PlayHangman::Draw(ostream & fout)
 		fout << "           | \n";
 		fout << "           O \n";
 		fout << "          -/-\n";
-		fout << "           \\n";
+		fout << "           \\\n";
 		_rightAsString(fout);
 	}
 	else if (_guessCount == 6)
-		{
+	{
 		fout << "===========+=\n";
 		fout << "           | \n";
 		fout << "           O \n";
 		fout << "          -/-\n";
-		fout << "          /\ \n";
+		fout << "          /\\ \n";
 		_rightAsString(fout);
-		}
+	}
 }
