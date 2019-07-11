8b1414a5360ba67480d8d99b60021a15ad75cfcf
Mathew Lister <36172965+AngryPorkchop11@users.noreply.github.com>
2019-07-10 20:09:15 -0700

    Refactor

--- HangMan/HangMan/HangMan.h
+++ HangMan/HangMan/HangMan.h
@@ -1,22 +1,25 @@
-#ifndef _HANGMAN
-#define _HANGMAN
-#include <string>				  
+#pragma once
+#include <string>
+
+using namespace std;
 class PlayHangman
 {
 public:
-	// Constructor: Set answer and build arrays to hold guesses and correct guesses
-	PlayHangman(string& answer);	
-	int GuessCheck(char& guess);
-	void ShowGuesses(ostream& fout);
-	bool Lose();
-	bool Win();
-	void _rightAsString(ostream & fout);
-	void Draw(ostream& fout);
+	//Constructor: Set answer and build arrays to hold wrong guesses and correct guesses
+	PlayHangman(string& answer);
+	bool GuessCheck(char& guess); // Checks if the guessed char is in the answer word
+	int checkMultiple(char guess); // Checks if player has already guessed a char
+	void ShowGuesses(ostream& fout); // Shows all the letters guessed
+	bool Lose(); // Checks if player lost
+	bool Win(); // Checks if player won
+	void _rightAsString(ostream& fout); // Writes the correct guessed letter as a string
+	void Draw(ostream& fout); // Draws hangman on how many wrong guesses
+
 private:
-	unsigned int _size;
-	unsigned int _guessCount;
-	string _answer;
-	char* _guess;
-	char* _right;
+	const int _endGame = 6; // 6 wrong guesses with draw hangman all the way - ending the game
+	unsigned int _size; // Size of the array based on the answer word
+	unsigned int _guessCount; // Number of incorrect guesses
+	string _answer; // Answer word
+	char* _wrong; // char array of wrong guesses
+	char* _right; // char array of right guesses
 };
-#endif
